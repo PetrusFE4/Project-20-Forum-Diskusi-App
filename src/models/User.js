0510c02639e6cd4f199b753d0fb654de2636e62c
Kyroline <rizky.w.dewantoro@gmail.com>
2024-06-08 00:41:56 +0700

    Added username removed first and last name

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,10 @@ import mongoose, { Schema } from 'mongoose'
 
 const schema = new Schema(
     {
-        firstname: { type: String, required: true },
-        lastname: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true, select: false },
+        activated: { type: Boolean, default: false, select: false},
         profile_picture: { type: String, default: null }
     },
     {
