17d06a0561b20feddc7f08d71a76cd4815e35433
Nawaf4224 <azzanawaf03@gmail.com>
2024-06-10 00:11:02 +0700

    Menambahkan HomePage

--- src/routes/BrowserRouter.jsx
+++ src/routes/BrowserRouter.jsx
@@ -1,11 +1,27 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from '../components/Navbar/Navbar'
 import Login from '../pages/login/Login'
 import Registrasi from '../pages/registrasi/Registrasi'
 import Forgetpassword from '../pages/forgetpassword/ForgetPassword'
 import MainLayout from '../layouts/MainLayout'
 import DiscussionDetail from '../pages/discussion_detail/DiscussionDetail'
+import About from '../routes/About'
+import Contact from '../routes/Contact'
+import Home from '../routes/home'
 
 const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/about',
+        element: <About />
+    },
+    {
+        path: '/contact',
+        element: <Contact />
+    },
     {
         path: '/login',
         element: <Login />
@@ -34,8 +50,10 @@ const routes = createBrowserRouter([
     }
 ])
 
+
 const BrowserRouter = () => {
     return <RouterProvider router={routes} />
 }
 
+<Navbar />
 export default BrowserRouter
