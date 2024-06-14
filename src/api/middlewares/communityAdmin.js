a4be19f1f0ff126aa4311857099c43bc8b31d6de
Kyroline <rizky.w.dewantoro@gmail.com>
2024-06-14 16:09:10 +0700

    Added report feature

---
+++ src/api/middlewares/communityAdmin.js
@@ -0,0 +1,13 @@
+import Community from "../models/Community"
+
+export const communityAdmin = async (req, res, next) => {
+    try {
+        const community = await Community.findOne({ _id: req.params.community_id, creator: req.user._id })
+        if (!community)
+            throw new Error()
+
+        next()
+    } catch (error) {
+        res.status(403).json({ message: 'Unauthorized action!' })
+    }
+}
---
+++ src/api/models/UserReport.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose'
+
+const schema = new Schema(
+    {
+        reporter: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+        reportee: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+        source: { type: Schema.Types.ObjectId, required: true },
+        report_type: { type: String, enum: ['post', 'reply'], required: true },
+        description: { type: String, required: true }
+    }
+)
+
+schema.index({ user: 1, source: 1 }, { unique: true })
+
+const UserReport = mongoose.model('UserReport', schema)
+
+export default UserReport
