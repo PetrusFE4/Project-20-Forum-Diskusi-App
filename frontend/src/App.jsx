89ed2ecd90a15dace7400fca39dbcb07b0c45a33
Petrus743 <petrusagung12@gmail.com>
2024-06-15 15:44:30 +0700

    npm install

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
+// *
 // import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './routes/home'
-import Dashboard from './pages/dashboard/Dashboard'
+import Home from "./routes/home";
+import Dashboard from "./pages/dashboard/Dashboard";
 import DashboardAdmin from "./pages/DashboardAdmin/DashboardAdmin";
 import UserData from "./pages/DashboardAdmin/UserData";
 import Communities from "./pages/Communities";
@@ -11,20 +12,19 @@ import Account from "./pages/Account";
 import Post from "./pages/Post";
 
 function App() {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<UserData />} />
-                {/* <Route path="/login" element={<Login />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserData />} />
+        {/* <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/account" element={<Account />} /> 
                 <Route path="/dashboard" element={<Dashboard />}/>
                  <Route path="/communities" element={<Communities />}/>
                 <Route path="/post" element={<Post />}/> */}
-            </Routes>
-        </BrowserRouter>
-    )
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
