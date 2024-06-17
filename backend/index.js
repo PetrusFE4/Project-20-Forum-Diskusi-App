1ba79c14466bb90e0e73d1a68bcba0e749a54ff3
Kyroline <rizky.w.dewantoro@gmail.com>
2024-06-17 15:08:28 +0700

    Added initialization config

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 import app from './src/api/app.js'
+import express from 'express'
+import fs from 'fs'
 import http from 'http'
 
 const port = process.env.PORT || 3000
 const host = process.env.HOST || 'http://localhost'
 
+app.use(express.static('public'))
 const server = http.createServer(app)
+
+var dirs = ['./public/uploads/', './public/uploads/tmp', './public/uploads/community', './public/uploads/post', './public/uploads/user']
+
+for (const dir of dirs) {
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir, { recursive: true });
+    }
+}
 
 server.listen(port, host, () => {
     console.log(`Server is running on ${host}:${port}`);
