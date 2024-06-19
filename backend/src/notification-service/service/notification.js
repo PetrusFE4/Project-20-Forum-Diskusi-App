557291daffdc603011d5edcdb860eae934733425
Kyroline <rizky.w.dewantoro@gmail.com>
2024-06-19 14:56:23 +0700

    Notification fix

--- backend/src/notification-service/service/notification.js
+++ backend/src/notification-service/service/notification.js
@@ -5,12 +5,19 @@ import Notification from '../schemas/Notification.js'
 import User from '../schemas/User.js'
 import UserFollower from '../schemas/UserFollower.js'
 import { WebSocket } from '../config/websocket.js'
+import mongoose from 'mongoose'
 
 export const ProcessNotification = async (msg) => {
     const messageJson = JSON.parse(msg.content.toString())
 
     if (messageJson.community) {
-        const notifiedUsers = await CommunityUser.find({ community: messageJson.community, notification: true })
+        // const notifiedUsers = await CommunityUser.find({ community: messageJson.community, notification: true })
+        const notifiedUsers = await CommunityUser.aggregate([
+            { $match: { community: mongoose.Types.ObjectId(new mongoose.Types.ObjectId(messageJson.community)) } },
+            {
+                $replaceRoot: { newRoot: '$user' }
+            }
+        ])
         const community = await Community.findOne({ _id: messageJson.community })
         const poster = await User.findOne({ _id: messageJson.post.user })
 
@@ -30,7 +37,13 @@ export const ProcessNotification = async (msg) => {
             WebSocket.to(`user:${user._id}`).emit('new_notification', JSON.stringify(notification))
         }
     } else {
-        const notifiedUsers = await UserFollower.find({ user: messageJson.post.user, notification: true })
+        const notifiedUsers = await UserFollower.aggregate([
+            { $match: { user: mongoose.Types.ObjectId(new mongoose.Types.ObjectId(messageJson.post.user)) } },
+            {
+                $replaceRoot: { newRoot: '$follower' }
+            }
+        ])
+        // const notifiedUsers = await UserFollower.find({ user: messageJson.post.user, notification: true })
         const poster = await User.findOne({ _id: messageJson.post.user })
 
         const notificationMessage = `${poster.username} created a new post in their profile. ${messageJson.post.title.length > 50 ? messageJson.post.title.slice(0, 50) + '...' : messageJson.post.title}`
@@ -48,7 +61,7 @@ export const ProcessNotification = async (msg) => {
 
             WebSocket.to(`user:${user._id}`).emit('new_notification', JSON.stringify(notification))
         }
-        
+
     }
     console.log(`New Message: ${msg.content.toString()}`)
 }
