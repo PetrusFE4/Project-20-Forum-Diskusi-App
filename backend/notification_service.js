ab83d98c0dee0a1fbcc34725181627ff7abfcadc
Kyroline <rizky.w.dewantoro@gmail.com>
2024-06-19 10:38:30 +0700

    Fixing cors

--- backend/notification_service.js
+++ backend/notification_service.js
@@ -6,7 +6,29 @@ import Run from './src/notification-service/app.js'
 const port = process.env.WS_PORT || 6420
 const host = process.env.WS_HOST || 'localhost'
 
-const server = http.createServer()
+const server = http.createServer((req, res) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Max-Age': 2592000, // 30 days
+        /** add other headers as per requirement */
+    };
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204, headers);
+        res.end();
+        return;
+    }
+
+    if (['GET', 'POST'].indexOf(req.method) > -1) {
+        res.writeHead(200, headers);
+        res.end('Hello World');
+        return;
+    }
+
+    res.writeHead(405, headers);
+    res.end(`${req.method} is not allowed for the request.`);
+})
 InitWebSocket(server)
 
 Run()
