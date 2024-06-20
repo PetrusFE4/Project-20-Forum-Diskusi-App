8839b7a61e5286b094811e02973da30c3e66fdf4
Kyroline <rizky.w.dewantoro@gmail.com>
2024-06-20 17:36:45 +0700

    fix path

--- backend/docs/mainSwaggerDoc.js
+++ backend/docs/mainSwaggerDoc.js
@@ -1,15 +1,20 @@
 import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const mainSwaggerDoc = JSON.parse(fs.readFileSync('./docs/main.json', 'utf8'));
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const authPaths = JSON.parse(fs.readFileSync('./docs/paths/auth.json', 'utf8'));
-const communityPaths = JSON.parse(fs.readFileSync('./docs/paths/community.json', 'utf8'));
-const postPaths = JSON.parse(fs.readFileSync('./docs/paths/post.json', 'utf8'));
-const replyPaths = JSON.parse(fs.readFileSync('./docs/paths/reply.json', 'utf8'));
+const mainSwaggerDoc = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'main.json'), 'utf8'));
 
-const postSchema = JSON.parse(fs.readFileSync('./docs/components/schemas/post.json', 'utf8'));
-const replySchema = JSON.parse(fs.readFileSync('./docs/components/schemas/reply.json', 'utf8'));
-const userSchema = JSON.parse(fs.readFileSync('./docs/components/schemas/user.json', 'utf8'));
+const authPaths = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'paths/auth.json'), 'utf8'));
+const communityPaths = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'paths/community.json'), 'utf8'));
+const postPaths = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'paths/post.json'), 'utf8'));
+const replyPaths = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'paths/reply.json'), 'utf8'));
+
+const postSchema = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'components/schemas/post.json'), 'utf8'));
+const replySchema = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'components/schemas/reply.json'), 'utf8'));
+const userSchema = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'components/schemas/user.json'), 'utf8'));
 
 mainSwaggerDoc.paths = {
     ...mainSwaggerDoc.paths,
